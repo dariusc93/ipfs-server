3d35dc73d5d9ec4d473f7eda1e8888de0ce479e7
Darius <darius.clark93@gmail.com>
2023-03-07 08:31:24 -0500

    chore: Load keypair from ipfs config or file with protobuf keypair

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,61 @@
-use std::error::Error;
+mod config;
+use std::{error::Error, path::PathBuf};
 
+use base64::{
+    alphabet::STANDARD,
+    engine::{general_purpose::PAD, GeneralPurpose},
+    Engine,
+};
 use clap::Parser;
 use rust_ipfs::{
     p2p::{IdentifyConfiguration, PeerInfo},
-    UninitializedIpfs,
+    IpfsOptions, Keypair, UninitializedIpfs,
 };
 use tokio::sync::Notify;
 
+use crate::config::IpfsConfig;
+
 #[derive(Debug, Parser)]
 #[clap(name = "ipfs-server")]
-struct Options {}
+struct Options {
+    #[clap(short, long)]
+    path: Option<PathBuf>,
+
+    #[clap(short, long)]
+    keypair: Option<PathBuf>,
+
+    #[clap(short, long)]
+    config: Option<PathBuf>,
+}
 
 #[tokio::main]
 #[allow(unreachable_code)]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let ipfs = UninitializedIpfs::new()
+    let opt = Options::parse();
+
+    let mut keypair: Option<Keypair> = None;
+
+    if let Some(path) = opt.config {
+        let config = IpfsConfig::load(path)?;
+        keypair = Some(config.identity.keypair()?);
+    }
+
+    if keypair.is_none() {
+        if let Some(path) = opt.keypair {
+            let keypair_data = zeroize::Zeroizing::new(tokio::fs::read_to_string(path).await?);
+            let engine = GeneralPurpose::new(&STANDARD, PAD);
+            let bytes = zeroize::Zeroizing::new(engine.decode(keypair_data.as_bytes())?);
+            keypair = Some(Keypair::from_protobuf_encoding(&bytes)?);
+        }
+    }
+
+    let mut options = IpfsOptions::default();
+
+    if let Some(keypair) = keypair {
+        options.keypair = keypair;
+    }
+
+    let mut uninitialized = UninitializedIpfs::with_opt(options)
         .enable_mdns()
         .enable_relay(true)
         .enable_relay_server(None)
@@ -25,9 +66,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
             push_update: true,
             cache: 200,
             ..Default::default()
-        })
-        .start()
-        .await?;
+        });
+
+    if let Some(path) = opt.path {
+        uninitialized = uninitialized.set_path(path);
+    }
+
+    let ipfs = uninitialized.start().await?;
 
     ipfs.default_bootstrap().await?;
     tokio::spawn({
