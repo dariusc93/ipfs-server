129dcb73ecef1f45b9e68a446126f553b7d53add
Darius <darius.clark93@gmail.com>
2023-03-10 00:40:14 -0500

    chore: Update ipfs to 0.3.3 and additional options - Added command line flags for adding relays, and listening addresses - Adjusted swarm and transport configuration

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use base64::{
 };
 use clap::Parser;
 use rust_ipfs::{
-    p2p::{IdentifyConfiguration, PeerInfo},
-    IpfsOptions, Keypair, UninitializedIpfs,
+    p2p::{IdentifyConfiguration, PeerInfo, SwarmConfig, TransportConfig},
+    IpfsOptions, Keypair, Multiaddr, Protocol, UninitializedIpfs,
 };
 use tokio::sync::Notify;
 
@@ -26,6 +26,12 @@ struct Options {
 
     #[clap(short, long)]
     config: Option<PathBuf>,
+
+    #[clap(short, long)]
+    listen_address: Vec<Multiaddr>,
+
+    #[clap(short, long)]
+    relays: Vec<Multiaddr>,
 }
 
 #[tokio::main]
@@ -58,21 +64,58 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut uninitialized = UninitializedIpfs::with_opt(options)
         .enable_mdns()
         .enable_relay(true)
-        .enable_relay_server(None)
         .enable_upnp()
         .disable_delay()
+        .set_swarm_configuration(SwarmConfig {
+            notify_handler_buffer_size: 32.try_into()?,
+            connection_event_buffer_size: 1024.try_into()?,
+            ..Default::default()
+        })
+        .set_transport_configuration(TransportConfig {
+            yamux_max_buffer_size: 16 * 1024 * 1024,
+            yamux_receive_window_size: 16 * 1024 * 1024,
+            mplex_max_buffer_size: usize::MAX / 2,
+            ..Default::default()
+        })
         .set_identify_configuration(IdentifyConfiguration {
             agent_version: "ipfs-server/0.1.0".into(),
             push_update: true,
             cache: 200,
             ..Default::default()
         });
+    
+    if !opt.listen_address.is_empty() {
+        for addr in opt.listen_address {
+            uninitialized = uninitialized.add_listening_addr(addr);
+        }
+    }
+
+    if opt.relays.is_empty() {
+        uninitialized = uninitialized.enable_relay_server(None);
+    }
 
     if let Some(path) = opt.path {
         uninitialized = uninitialized.set_path(path);
     }
 
     let ipfs = uninitialized.start().await?;
+
+    if !opt.relays.is_empty() {
+        for relay in opt.relays {
+            if let Err(e) = ipfs.dial(relay.clone()).await {
+                println!("Error dialing relay: {e}");
+                continue;
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+            if let Err(e) = ipfs
+                .add_listening_address(relay.with(Protocol::P2pCircuit))
+                .await
+            {
+                println!("Error listening on relay circuit: {e}");
+                continue;
+            }
+        }
+    }
 
     ipfs.default_bootstrap().await?;
     tokio::spawn({
